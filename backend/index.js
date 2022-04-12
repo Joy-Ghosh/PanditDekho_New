cb4c013b4eefc09f71c313810528060b77dc54be
Adwait Narayan Pradhan <adwait@fleapo.com>
2022-04-12 09:46:27 +0530

    basic auth with cookies implemented, testing needed

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 
 const { mongoose } = require("./server.js");
 var Client_signup_Route = require("./routes/client_signup.route.js");
-
+const CookieParser = require("cookie-parser");
 var app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(CookieParser());
 const port = process.env.PORT || 4100;
 app.listen(port, () => {
   console.log("Connected to port " + port);
